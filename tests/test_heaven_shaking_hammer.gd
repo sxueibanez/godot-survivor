9b34f55fc1f8326d026ca301ad1c0a45180607da
sxueibanez <siban002@odu.edu>
2026-09-23 07:44:45 -0400

    优化

--- tests/test_heaven_shaking_hammer.gd
+++
@@ -1,9 +0,0 @@
-extends SceneTree
-
-
-func _initialize() -> void:
-	var upgrade := load("res://resources/upgrades/heaven_shaking_hammer_extra_wave.tres") as AbilityUpgrade
-	assert(upgrade != null)
-	assert(upgrade.max_quantity == 4)
-	assert(load("res://scenes/ability/heaven_shaking_hammer_controller/heaven_shaking_hammer_controller.tscn") != null)
-	quit()
--- tests/test_level_3_mine.gd
+++
@@ -1,7 +0,0 @@
-extends SceneTree
-
-
-func _init() -> void:
-	assert(load("res://scenes/environment/level_3_mine.gd") is Script)
-	assert(load("res://assets/environment/tilemap_packed.png") is Texture2D)
-	quit()
